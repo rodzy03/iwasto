32de882249f1baceccf10ad977516c8975b4bb3e
Rodel <duterterodelb@gmail.com>
2021-06-03 11:49:05 -0400

    modify z patrol

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    
-    // return view('home');
-});
-
 // Mobile routes
 Route::post('/get-zipcodes','MobileController@get_zipcodes')->name('get-zipcodes');
 Route::post('/get-barangays','MobileController@get_barangays')->name('get-barangays');
